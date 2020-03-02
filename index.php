16434f0593c22174d872a7e265b214dc775ab86a
dimitry <dimiw1337@hotmail.com>
2020-03-02 09:27:15 -0500

    email working i think ( cant check it ) so done.

--- index.php
+++ index.php
@@ -9,10 +9,16 @@ require 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
+$from = "x@gmail.com";
+$to = "dimiw1337@hotmail.com";
+$subject = "Logger \Monolog\Handler\NativeMailerHandler";
+$body = 'body';
+
 if  (isset($_GET ['type'])) {
     $get = $_GET ['type'];
     $log = new Logger('logger');
     switch ($get) {
+
         case 'DEBUG':
             $log->pushHandler(new StreamHandler(__DIR__ . '/logs/blue.log', Logger::DEBUG));
         // add records to the log
@@ -35,6 +41,7 @@ if  (isset($_GET ['type'])) {
             $log->pushHandler(new StreamHandler(__DIR__ . '/logs/yellow.log', Logger::WARNING));
             // add records to the log
             $log->warning($_GET ['message']);
+            $nativeMailerHandler = new \Monolog\Handler\NativeMailerHandler($to, $subject, $from,logger::WARNING);
             break;
 
         case  'ERROR':
@@ -59,6 +66,8 @@ if  (isset($_GET ['type'])) {
             $log->pushHandler(new StreamHandler(__DIR__ . '/logs/black.log', Logger::EMERGENCY));
             // add records to the log
             $log->emergency($_GET ['message']);
+            $nativeMailerHandler = new \Monolog\Handler\NativeMailerHandler($to, $subject, $from,logger::EMERGENCY);
+            break;
     }
 }
 require 'buttons.html';
